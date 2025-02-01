5a2b468dee84f32afbeb4ac478d9d845a4c496e5
warlyn <warlyn200@gmail.com>
2025-02-01 09:44:27 -0400

    Refactor and split `store` method for DGII requests.
    Separate `store` logic into `store` and `storeEcf` methods. Replaced inline XML processing with a view-based rendering approach and updated associated resources for better modularity.

--- app/Http/Controllers/DgiiRequestController.php
+++ app/Http/Controllers/DgiiRequestController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Actions\DgiiRequest\GenerateXML;
 use App\Actions\DgiiRequest\SignFCXML;
-use App\Actions\DgiiRequest\SignXML;
+use App\Actions\SignXML;
 use App\Enums\DgiiRequest\StatusEnum;
 use App\Enums\DgiiRequest\TypeEnum;
 use App\Http\Requests\DgiiRequest\StoreDgiiRequest;
+use App\Http\Requests\DgiiRequest\StoreEcfRequest;
+use App\Http\Resources\DgiiRequest\EcfResource;
 use App\Http\Resources\DgiiRequestResource;
 use App\Models\DgiiRequest;
 use Carbon\Carbon;
@@ -25,43 +27,35 @@ class DgiiRequestController extends Controller
         return DgiiRequestResource::collection($records);
     }
 
+    public function store(StoreDgiiRequest $request)
+    {
+        $data = $request->validated();
+
+        $record = DgiiRequest::create($data);
+
+        return new DgiiRequestResource($record);
+    }
+
     /**
      * Store a newly created resource in storage.
      *
      * @throws \Exception
      */
-    public function store(StoreDgiiRequest $request)
+    public function storeEcf(StoreEcfRequest $request)
     {
         $data = $request->validated();
 
-        $sequenceNumber = $data['IdDoc']['eNCF'];
-        $total = $data['Totales']['MontoTotal'] ?? 20;
-        $typeValue = (int) substr($sequenceNumber, 1, 2);
-        $type = TypeEnum::getFromValue($typeValue);
-
-        $xml = GenerateXML::run($type, $data);
+        $xml = view('dgii-request.ecf', $data)->render();
         $signedXml = SignXML::run($xml);
-
-        $xmlClass = simplexml_load_string($signedXml);
-        $securityCode = substr((string) $xmlClass?->Signature?->SignatureValue, 0, 6);
-        $signatureDate = Carbon::parse((string) $xmlClass?->FechaHoraFirma);
-
-        if ($type == TypeEnum::consumeInvoice and $total <= config('dgii.fc_limit')) {
-            $signedXml = SignFCXML::run($data, $securityCode);
-        }
 
         $record = DgiiRequest::create([
             'status' => StatusEnum::inProcess,
-            'type' => $type,
-            'sequence_number' => $sequenceNumber,
-            'security_code' => $securityCode,
-            'signature_date' => $signatureDate,
             'signed_xml' => $signedXml,
             'request' => $data,
             'user_id' => $request->user()->id,
         ]);
 
-        return new DgiiRequestResource($record);
+        return new EcfResource($record);
     }
 
     /**
