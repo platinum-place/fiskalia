bb71e8e426cf06cb6e5d28b02c95840fc140ecd4
warlyn <warlyn200@gmail.com>
2025-02-01 09:43:12 -0400

    Add route for creating ECF in DGII requests
    Introduced a new POST route 'dgii-requests/create-ecf' handled by the `storeEcf` method in `DgiiRequestController`. This allows creating ECF entries while maintaining existing API resource functionality.

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => [\App\Http\Middleware\AuthenticateOnceWithBasicAuth::class]], function () {
+    Route::post('dgii-requests/create-ecf', [\App\Http\Controllers\DgiiRequestController::class, 'storeEcf'])->name('dgii-requests.create-ecf');
     Route::apiResource('dgii-requests', \App\Http\Controllers\DgiiRequestController::class);
 });
